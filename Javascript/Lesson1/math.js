142be936c838b14c59221f700230425b634bcc08
Hackerpro335 <iurewi4.tix@gmail.com>
2025-07-16 21:49:41 +0300

    Добавила Lesson1 в JS

---
+++ Javascript/Lesson1/math.js
@@ -0,0 +1,31 @@
+/*<script src="intro.js" type="text/javascript"></script> - существует тип type*/
+/*alert(planetEarthName)*/
+/*Task1*/
+//let userNumber = prompt('Введите какое-нибудь число');
+/*let result = userNumber * userNumber; /* ** - возведение в степень*/
+//let result = userNumber ** 2;
+
+//alert(`Результат вычисления ${result}`);
+/*Task2*/
+/* Результат работы prompt - это строка. "4" + "4"/2. 
+Если + перед prompt не ставить, то будет:
+Деление имеет более высокий приоритет чем сложение. Строка 4 делят на 2. 4 - преобразовано в число. А 4 
+Строка + число - JS склеит и получится 42
++prompt - преобразует строковое значение в числовое, а минус в отрицательное.*/
+ 
+//let userNumber1 = +prompt('Введите первое число');
+//let userNumber2 = +prompt('Введите второе число');
+/*let result = (userNumber1+userNumber1)/2;
+alert(`Среднее арифметическое ${result}`)*/
+//let userNumber1 = Number(prompt('Введите первое число');
+//let userNumber2 = +prompt('Введите второе число');
+
+//alert (userNumber1 + userNumber2)/2);
+
+/*Task3*/
+/*const MILE = 0.621371;
+let userkm = +prompt ('Введите количество км');
+result = userkm * MILE;
+alert (`Итого милей: ${result}`)*/
+/* Введение, до логических операторов, про строгий режим + ДЗ к ним */ 
+/*Сложное ветвление, операторы и функции*/
---
+++ Javascript/Lesson1/pz1.js
@@ -0,0 +1,22 @@
+/*Task 1*/
+let admin;
+let name;
+
+name = "Джон";
+admin = name;
+
+alert(admin);
+/*Task 2*/
+/* Название переменных должно быть понятно. Если нужно название планеты лучше написать planetEarthName*/
+let planetEarthName = 'Земля';
+let currentUserOnline = {};
+/*Task 3*/
+/* Здесь изначально знаем переменную, значит буквы д/б большие в названии константы*/
+const BIRTHDAY = '18.04.1982';
+/*Здесь значение высчитывается с помощью someCode, т.е. изначально не задано, поэтому переменная название пишут с маленькой буквы*/
+const age = someCode(birthday);
+/*Task 4*/
+let name = "Ilya"
+alert(`Hello ${1}`);
+alert(`Hello ${"name"}`);
+alert(`Hello ${name}`);
