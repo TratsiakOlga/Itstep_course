aa063c36518cdd1a8f5f8d23f72049eea8acd715
KudatoNado <kodinetsdaniil@gmail.com>
2025-10-13 21:47:54 +0300

    верстка от начала до конца.часть3

--- Artefacts/js/custom.js
+++ Artefacts/js/custom.js
@@ -1,9 +1,17 @@
+let totalPages = 0;
+let currentPageIndex = 1;
+
+function setSliderPagination(){
+    $("#slider-pagination"). html (`${currentPageIndex} из ${totalPages}`);
+}
+
 $(document).ready(function(){
     let options = {
         items: 4,
         margin: 30,
         nav: true, 
-        dots: false,    
+        dots: false,
+        slideBy:4,    
     }
 
     let owl = $('.owl-carousel');
@@ -11,10 +19,32 @@ $(document).ready(function(){
 
     $("#slider-left").click(function(){
         owl.trigger('prev.owl.carousel');
+        
+        if(currentPageIndex > 1){
+            currentPageIndex--;
+        }
+
+        setSliderPagination();
     });
 
     $("#slider-right").click(function(){
         owl.trigger('next.owl.carousel');
+        
+        if(currentPageIndex < totalPages){
+            currentPageIndex++;
+        }
+
+        setSliderPagination();
     });
 
+    let totalCarts = $(".owl-item").length;
+    
+    totalPages = Math.ceil(totalCarts / options.items);
+
+    setSliderPagination();
   });
+
+
+  function validate() {
+      return true;
+  }
