dd61f635c3f8211ed15fd1ddc7c0e308c3837811
KudatoNado <kodinetsdaniil@gmail.com>
2025-10-20 21:50:54 +0300

    верстка с начала до конца. часть 4

--- Artefacts/js/custom.js
+++ Artefacts/js/custom.js
@@ -25,6 +25,8 @@ $(document).ready(function(){
         }
 
         setSliderPagination();
+
+        
     });
 
     $("#slider-right").click(function(){
@@ -42,9 +44,72 @@ $(document).ready(function(){
     totalPages = Math.ceil(totalCarts / options.items);
 
     setSliderPagination();
+
+//$("#form-login").validate(); - если используем библиотеку jqueary
+//Справочно!!! Валидация с помощью bootstrap (до function validate(event))
+    const loginForm = document.querySelector('#form-login');
+
+    if(loginForm) {
+        loginForm.addEventListener('submit', function(event){
+            if (!this.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+    
+            this.classList.add('was-validated');
+        });
+    }
   });
 
 
-  function validate() {
-      return true ;
-  }
+  function validate(event) {
+      let isValid = true;    
+      let form = document.forms[0];
+      let name = form.elements[0];
+      let email = form.elements[1];
+  
+      let name_regex = /^[A-Za-zА-Яа-я]{2,20}\s*[A-Za-zА-Яа-я]{0,20}$/; //говори, что можно писать Фамилию, имя
+      let email_regex = /^[a-z_\-0-9]+@[a-z_\-0-9]+\.[a-z]{2,3}$/;
+
+      name.classList.remove('is-invalid');
+      if(!name_regex.test(name.value)){
+          name.classList.add('is-invalid');
+          isValid = false;
+        }
+
+        email.classList.remove('is-invalid');
+        if(!email_regex.test(email.value)){
+            email.classList.add('is-invalid');
+            isValid = false;
+        } 
+
+        return isValid;
+    }
+    //console.dir(form);
+    //console.dir(name);
+    //console.dir(document.forms);
+    
+    //let str = "Hi! My name is Edgar. I'm 42 years old"; - это для примера заводилось
+    
+    //let regex = /[A-Za-zА-Яа-я]{2,20}/; //что должно содержаться английские от A-Z и маленькие a-z, также русские от А-Я и а-я (большие и маленькие), и от 2 до 20 букв
+
+    /*let result = regex.text(str);
+
+    console.log(result)*/
+    //let regex = /\sEdgar/; - указывает есть ли данное выражение в тексте //\s - обозначение пробела.
+    /*let regex = /\d/; - проверяет есть ли в тексте цифры
+    let regex = /\d{2}/; // указывает: находим совпадения где есть подряд две цифры {2} - указывает, что нужно искать 2 цифры. 
+
+    let regex = /\d{2,}/; // горит или два, или больше
+    let regex = /\d\s\d\s\d\s\d/; //четыре цифры, четыре пробела между цифрами. Если цифры, например 4 2 3 5
+    let result = regex.test(str);*/
+    /* Проверяем есть ли имя в строке:
+    if(result){
+        console.log(result);
+    }*/
+
+    //console.log(result);
+    //console.dir(regex);
+/*return false;
+}*/
+    
