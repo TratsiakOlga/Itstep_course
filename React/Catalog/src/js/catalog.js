12c7ce89e40fc7e07d9fe0948b0e3087c9b5aa70
TratsiakOlga <tratsiak_olga@mail.ru>
2026-02-13 22:42:27 +0300

    обновила данные

--- React/Catalog/src/js/catalog.js
+++ React/Catalog/src/js/catalog.js
@@ -1,14 +1,11 @@
-import '../scss/index.scss';
-import { createRoot } from 'react-dom/client';
-import App from './addProduct/App';
-import ProductFotm from './addProduct/ProductForm';
+import '../scss/catalog.scss';
 
-const appElement = document.getElementById('app');
-const newAppElement = document.getElementById('app-2');
+import {createRoot} from 'react-dom/client';
 
+import Catalog from './components/Catalog.js';
 
-const root = createRoot(appElement);
-const newRoot = createRoot(newAppElement);
+let element = document.querySelector(".catalog");
 
-root.render(<App />);
-newRoot.render(<ProductFotm />);
+let rootElement = createRoot(element);
+
+rootElement.render(<Catalog />);
